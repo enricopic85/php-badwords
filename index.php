a004d8e551294aca69b68da39484ab91d6442b1c
enricopic85 <enricodev85@gmail.com>
2022-02-14 15:06:29 +0100

    creazione progetto

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $paragrafo='sotto la panca la capra canta, sopra la panca la capra crepa';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $paragrafo; echo strlen($paragrafo) ?>
+</body>
+</html>
